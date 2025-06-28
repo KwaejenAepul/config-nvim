697c2ae80712fea09a8a1112bf45cafd66700bad
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2025-06-28 15:37:32 +0200

    add auto update to Lazy

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require("config.lazy")
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
+vim.api.nvim_create_autocmd("VimEnter",{callback=function()require"lazy".update({show = false})end})
+
 local gdproject = io.open(vim.fn.getcwd()..'/project.godot', 'r')
 if gdproject then
     io.close(gdproject)
