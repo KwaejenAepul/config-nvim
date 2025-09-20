2f184d0a53495fba32c9715fd54a5fc170e4de32
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2025-09-20 14:10:36 +0200

    lsp config changes to do it the 0.11 way

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -149,7 +149,7 @@ return {
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      require('lspconfig').gdscript.setup({capabilities=capabilities})
+      vim.lsp.enable("gdscript",{capabilities={capabilities}})
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -212,17 +212,7 @@ return {
 
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
+        automatic_installation = false
       }
     end,
   }
