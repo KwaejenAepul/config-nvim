e4d1171f5384773574e86bc26f967c3d6c2297fc
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2025-10-10 14:48:34 +0200

    removed and changed some keybinds I never use

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,24 +11,13 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- snack keybinds
--- Top Pickers & Explorer
-vim.keymap.set("n", "<leader><space>", function() Snacks.picker.smart() end, {desc = "Smart Find Files"})
-vim.keymap.set("n", "<leader>,", function() Snacks.picker.buffers() end, {desc = "Buffers"})
-vim.keymap.set("n", "<leader>/", function() Snacks.picker.grep() end, {desc = "Grep"} )
-vim.keymap.set("n", "<leader>:", function() Snacks.picker.command_history() end, {desc = "Command History"})
-vim.keymap.set("n", "<leader>e", function() Snacks.explorer() end, {desc = "File Explorer"})
---find
 vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end, {desc = "Buffers"})
-vim.keymap.set("n", "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, {desc = "Find Config File"})
 vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, {desc = "Find Files"})
-vim.keymap.set("n", "<leader>fg", function() Snacks.picker.git_files() end, {desc = "Find Git Files"})
+vim.keymap.set("n", "<leader>fg", function() Snacks.picker.grep() end, {desc = "Find Git Files"})
 vim.keymap.set("n", "<leader>fp", function() Snacks.picker.projects() end, {desc = "Projects"})
 vim.keymap.set("n", "<leader>fr", function() Snacks.picker.recent() end, {desc = "Recent"})
---Grep
-vim.keymap.set("n", "<leader>sb", function() Snacks.picker.lines() end, {desc = "Buffer Lines"})
-vim.keymap.set("n", "<leader>sB", function() Snacks.picker.grep_buffers() end, {desc = "Grep Open Buffers"})
-vim.keymap.set("n", "<leader>sg", function() Snacks.picker.grep() end, {desc = "Grep"})
-vim.keymap.set("n", "<leader>sw", function() Snacks.picker.grep_word() end, {desc = "Visual selection or word"})
+vim.keymap.set("n", "<leader>sb", function() Snacks.picker.grep_buffers() end, {desc = "Grep Open Buffers"})
+vim.keymap.set("n", "<leader>sg", function() Snacks.picker.git_files() end, {desc = "Grep"})
 -- LSP
 vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, {desc = "Goto Definition"})
 vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end, {desc = "Goto Declaration"})
