f16e6fbb4f859a606414ccc52c01c4f32d4fd143
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2025-11-11 09:47:49 +0100

    changed theme just because

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 
 vim.api.nvim_create_autocmd("VimEnter",{callback=function()require"lazy".update({show = false})end})
-
+vim.cmd[[colorscheme nord]]
 local gdproject = io.open(vim.fn.getcwd()..'/project.godot', 'r')
 if gdproject then
     io.close(gdproject)
