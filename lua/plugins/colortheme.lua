8d1bad29ec37c22afb384f5c404fc2fae9a2a7f1
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2026-05-08 21:41:43 +0200

    godot stuff

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,3 +1,13 @@
 return {'NTBBloodbath/doom-one.nvim',priority = 1000,}
 
 --return{ "ellisonleao/gruvbox.nvim", priority = 1000}
+--return{
+--  "navarasu/onedark.nvim",
+--  priority = 1000, -- make sure to load this before all the other start plugins
+--  config = function()
+--    require('onedark').setup {
+--      style = 'darker'
+--    }
+--    require('onedark').load()
+--  end
+--}
